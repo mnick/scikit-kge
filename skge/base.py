384322f3e9588b4ec783de525b9b7bab286399af
Maximilian Nickel <mnick@mit.edu>
2016-02-04 21:26:07 -0500

    change Model class to support Parameter objects

--- skge/base.py
+++ skge/base.py
@@ -1,37 +1,74 @@
 import numpy as np
 from numpy.random import shuffle
-import scipy.sparse as sp
 from collections import defaultdict
-from skge.param import AdaGradUpdate
+from skge.param import Parameter, AdaGrad
 import timeit
 import pickle
 
 _cutoff = 30
 
 _DEF_NBATCHES = 100
-_DEF_CALLBACK = None
+_DEF_POST_EPOCH = []
 _DEF_LEARNING_RATE = 0.1
 _DEF_SAMPLE_FUN = None
 _DEF_MAX_EPOCHS = 1000
 _DEF_MARGIN = 1.0
 
 
-class Model(object):
+class Config(object):
 
-    def __init__(self, *args, **kwargs):
-        super(Model, self).__init__(*args, **kwargs)
+    def __init__(self, model, trainer):
+        self.model = model
+        self.trainer = trainer
 
     def __getstate__(self):
         return {
-            'max_epochs': self.max_epochs,
-            'nbatches': self.nbatches,
-            'learning_rate': self.learning_rate,
-            'init': self.init,
-            'epoch': self.epoch
+            'model': self.model,
+            'trainer': self.trainer
+        }
+
+
+class Model(object):
+    """
+    Base class for all Knowledge Graph models
+
+    Implements basic setup routines for parameters and serialization methods
+
+    Subclasses need to implement:
+    - scores(self, ss, ps, os)
+    - _gradients(self, xys) for StochasticTrainer
+    - _pairwise_gradients(self, pxs, nxs) for PairwiseStochasticTrainer
+    """
+
+    def __init__(self, *args, **kwargs):
+        #super(Model, self).__init__(*args, **)
+        self.params = {}
+        self.hyperparams = {}
+        self.add_hyperparam('init', kwargs.pop('init', 'nunif'))
+
+    def add_param(self, param_id, shape, post=None, value=None):
+        if value is None:
+            value = Parameter(shape, self.init, name=param_id, post=post)
+        setattr(self, param_id, value)
+        self.params[param_id] = value
+
+    def add_hyperparam(self, param_id, value):
+        setattr(self, param_id, value)
+        self.hyperparams[param_id] = value
+
+    def __getstate__(self):
+        return {
+            'hyperparams': self.hyperparams,
+            'params': self.params
         }
 
     def __setstate__(self, st):
-        self.__dict__ = st
+        self.params = {}
+        self.hyperparams = {}
+        for pid, p in st['params'].items():
+            self.add_param(pid, None, None, value=p)
+        for pid, p in st['hyperparams'].items():
+            self.add_hyperparam(pid, p)
 
     def save(self, fname, protocol=pickle.HIGHEST_PROTOCOL):
         with open(fname, 'wb') as fout:
@@ -45,21 +82,47 @@ class Model(object):
 
 
 class StochasticTrainer(object):
+    """
+    Stochastic gradient descent trainer with scalar loss function.
+
+    Models need to implement
+
+    _gradients(self, xys)
+
+    to be trained with this class.
+
+    """
 
     def __init__(self, *args, **kwargs):
-        self.max_epochs = kwargs.pop('max_epochs', _DEF_MAX_EPOCHS)
-        self.nbatches = kwargs.pop('nbatches', _DEF_NBATCHES)
-        self.callback = kwargs.pop('callback', _DEF_CALLBACK)
-        self.learning_rate = kwargs.pop('learning_rate', _DEF_LEARNING_RATE)
+        self.model = args[0]
+        self.hyperparams = {}
+        self.add_hyperparam('max_epochs', kwargs.pop('max_epochs', _DEF_MAX_EPOCHS))
+        self.add_hyperparam('nbatches', kwargs.pop('nbatches', _DEF_NBATCHES))
+        self.add_hyperparam('learning_rate', kwargs.pop('learning_rate', _DEF_LEARNING_RATE))
+
+        self.post_epoch = kwargs.pop('post_epoch', _DEF_POST_EPOCH)
         self.samplef = kwargs.pop('samplef', _DEF_SAMPLE_FUN)
-        self.param_updater = kwargs.pop('param_update', AdaGradUpdate)
-        self.init = kwargs.pop('init', 'randn')
+        pu = kwargs.pop('param_update', AdaGrad)
+        self._updaters = {
+            key: pu(param, self.learning_rate)
+            for key, param in self.model.params.items()
+        }
+
+    def __getstate__(self):
+        return self.hyperparams
+
+    def __setstate__(self, st):
+        for pid, p in st['hyperparams']:
+            self.add_hyperparam(pid, p)
+
+    def add_hyperparam(self, param_id, value):
+        setattr(self, param_id, value)
+        self.hyperparams[param_id] = value
 
     def fit(self, xs, ys):
-        self._init_factors(xs, ys)
         self._optim(list(zip(xs, ys)))
 
-    def pre_epoch(self):
+    def _pre_epoch(self):
         self.loss = 0
 
     def _optim(self, xys):
@@ -82,8 +145,9 @@ class StochasticTrainer(object):
                 self._process_batch(bxys)
 
             # check callback function, if false return
-            if self.callback is not None and not self.callback(self):
-                break
+            for f in self.post_epoch:
+                if not f(self):
+                    break
 
     def _process_batch(self, xys):
         # if enabled, sample additional examples
@@ -94,23 +158,48 @@ class StochasticTrainer(object):
             self._prepare_batch_step(xys)
 
         # take step for batch
-        res = self._batch_gradients(xys)
-        self._batch_step(res)
+        grads = self.model._gradients(xys)
+        self._batch_step(grads)
+
+    def _batch_step(self, grads):
+        for paramID in self._updaters.keys():
+            self._updaters[paramID](*grads[paramID])
 
 
 class PairwiseStochasticTrainer(StochasticTrainer):
+    """
+    Stochastic gradient descent trainer with pairwise ranking loss functions.
+
+    Models need to implement
+
+    _pairwise_gradients(self, pxs, nxs)
+
+    to be trained with this class.
+
+    """
+
 
     def __init__(self, *args, **kwargs):
         super(PairwiseStochasticTrainer, self).__init__(*args, **kwargs)
-        self.margin = kwargs.pop('margin', _DEF_MARGIN)
+        self.model.add_hyperparam('margin', kwargs.pop('margin', _DEF_MARGIN))
 
-    def __getstate__(self):
-        st = super(PairwiseStochasticTrainer, self).__getstate__()
-        st.update({'margin': self.margin})
-        return st
+    def fit(self, xs, ys):
+        if self.samplef is None:
+            pidx = np.where(np.array(ys) == 1)[0]
+            nidx = np.where(np.array(ys) != 1)[0]
+            pxs = [xs[i] for i in pidx]
+            self.nxs = [xs[i] for i in nidx]
+            self.pxs = int(len(self.nxs) / len(pxs)) * pxs
+            xys = list(range(min(len(pxs), len(self.nxs))))
+            self._optim(xys)
+        else:
+            self._optim(list(zip(xs, ys)))
 
     def pre_epoch(self):
         self.nviolations = 0
+        if self.samplef is None:
+            shuffle(self.pxs)
+            shuffle(self.nxs)
 
     def _process_batch(self, xys):
         pxs = []
@@ -122,25 +211,18 @@ class PairwiseStochasticTrainer(StochasticTrainer):
                     pxs.append(xy)
                     nxs.append(nx)
             else:
-                if xy[1] == 1:
-                    pxs.append((xy[0], 1))
-                else:
-                    nxs.append((xy[0], -1))
-
-        if self.samplef is None:
-            pidx, nidx = np.arange(len(pxs)), np.arange(len(nxs))
-            pidx, nidx = np.meshgrid(pidx, nidx)
-            pxs = [pxs[i] for i in pidx.flatten()]
-            nxs = [nxs[i] for i in nidx.flatten()]
+                pxs.append((self.pxs[xy], 1))
+                nxs.append((self.nxs[xy], 1))
 
         # take step for batch
         if hasattr(self, '_prepare_batch_step'):
             self._prepare_batch_step(pxs, nxs)
-        res = self._batch_gradients(pxs, nxs)
+        grads = self.model._pairwise_gradients(pxs, nxs)
 
         # update if examples violate margin
-        if res is not None:
-            self._batch_step(res)
+        if grads is not None:
+            self.nviolations += self.model.nviolations
+            self._batch_step(grads)
 
 
 class PredicateAlgorithmMixin:
@@ -172,11 +254,6 @@ class PredicateMarginAlgorithmMixin(PredicateAlgorithmMixin):
         self._prepare_model()
 
 
-# def ada_step(g, g2, eta):
-#     H = np.maximum(np.sqrt(g2), 1e-6)
-#     return eta * g / H
-
-
 def sigmoid(fs):
     # compute elementwise gradient for sigmoid
     for i in range(len(fs)):
@@ -187,24 +264,3 @@ def sigmoid(fs):
         else:
             fs[i] = 1.0 / (1 + np.exp(-fs[i]))
     return fs[:, np.newaxis]
-
-
-def to_tensor(xs, ys, sz):
-    T = [sp.lil_matrix((sz[0], sz[1])) for _ in range(sz[2])]
-    for i in range(len(xs)):
-        i, j, k = xs[i]
-        T[k][i, j] = ys[i]
-    return T
-
-
-def init_nvecs(xs, ys, sz, rank, with_T=False):
-    from scipy.sparse.linalg import eigsh
-
-    T = to_tensor(xs, ys, sz)
-    T = [Tk.tocsr() for Tk in T]
-    S = sum([T[k] + T[k].T for k in range(len(T))])
-    _, E = eigsh(sp.csr_matrix(S), rank)
-    if not with_T:
-        return E
-    else:
-        return E, T
