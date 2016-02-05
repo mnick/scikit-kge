6a99bca64b540beb4208e26cff991f3a028bdd44
Maximilian Nickel <mnick@mit.edu>
2016-02-04 21:34:59 -0500

    refactor transe to new model structure

--- skge/transe.py
+++ skge/transe.py
@@ -1,67 +1,42 @@
 import numpy as np
-from skge.base import Model, PairwiseStochasticTrainer
+from skge.base import Model
 from skge.util import grad_sum_matrix, unzip_triples
-from skge.param import ParamInit, normalize
+from skge.param import normalize
 
 
 class TransE(Model):
+    """
+    Translational Embeddings of Knowledge Graphs
+    """
 
     def __init__(self, *args, **kwargs):
         super(TransE, self).__init__(*args, **kwargs)
-        self.sz = args[0]
-        self.ncomp = args[1]
-        self.l1 = kwargs.pop('l1', True)
+        self.add_hyperparam('sz', args[0])
+        self.add_hyperparam('ncomp', args[1])
+        self.add_hyperparam('l1', kwargs.pop('l1', True))
+        self.add_param('E', (self.sz[0], self.ncomp), post=normalize)
+        self.add_param('R', (self.sz[2], self.ncomp))
 
-    def __getstate__(self):
-        st = super(TransE, self).__getstate__()
-        st.update({
-            'sz': self.sz,
-            'ncomp': self.ncomp,
-            'E': self.E,
-            'R': self.R,
-            'l1': self.l1
-        })
-        return st
-
-    def _scores(self, ss, ps, os):
+    def scores(self, ss, ps, os):
+        score = self.E[ss] + self.R[ps] - self.E[os]
         if self.l1:
-            score = np.abs(self.E[ss] + self.R[ps] - self.E[os])
+            score = np.abs(score)
         else:
-            score = (self.E[ss] + self.R[ps] - self.E[os]) ** 2
+            score = score ** 2
         return -np.sum(score, axis=1)
 
-    def _init_factors(self, xs, ys):
-        pinit = ParamInit(self.init)
-        self.E = pinit((self.sz[0], self.ncomp))
-        self.R = pinit((self.sz[2], self.ncomp))
-        self.E = normalize(self.E)
-        self.R = normalize(self.R)
-
-        self.ups = [
-            self.param_updater(self.E, self.learning_rate, normalize),
-            self.param_updater(self.R, self.learning_rate)
-        ]
-
-    def _batch_step(self, res):
-        ge, gr, eidx, ridx = res
-        self.ups[0](ge, eidx)
-        self.ups[1](gr, ridx)
-
-
-class PairwiseStochasticTransE(TransE, PairwiseStochasticTrainer):
-
-    def _batch_gradients(self, pxs, nxs):
+    def _pairwise_gradients(self, pxs, nxs):
         # indices of positive triples
         sp, pp, op = unzip_triples(pxs)
         # indices of negative triples
         sn, pn, on = unzip_triples(nxs)
 
-        pscores = self._scores(sp, pp, op)
-        nscores = self._scores(sn, pn, on)
+        pscores = self.scores(sp, pp, op)
+        nscores = self.scores(sn, pn, on)
         ind = np.where(nscores + self.margin > pscores)[0]
 
         # all examples in batch satify margin criterion
-        self.nviolations += len(ind)
+        self.nviolations = len(ind)
         if len(ind) == 0:
             return
 
@@ -83,12 +58,11 @@ class PairwiseStochasticTransE(TransE, PairwiseStochasticTrainer):
         else:
             raise NotImplementedError()
 
-        # role gradients
+        # entity gradients
         eidx, Sm, n = grad_sum_matrix(sp + op + sn + on)
-        ge = Sm.dot(np.vstack((
-            pg, -pg, ng, -ng
-        ))) / n
+        ge = Sm.dot(np.vstack((pg, -pg, ng, -ng))) / n
 
+        # relation gradients
         ridx, Sm, n = grad_sum_matrix(pp + pn)
         gr = Sm.dot(np.vstack((pg, ng))) / n
-        return ge, gr, eidx, ridx
+        return {'E': (ge, eidx), 'R': (gr, ridx)}
