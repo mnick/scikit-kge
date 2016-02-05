65de1bed8536dbeee40ee98bd486f1d95f6a8d2a
Maximilian Nickel <mnick@mit.edu>
2016-02-05 15:02:11 -0500

    add __init__.py for skge

---
+++ skge/__init__.py
@@ -0,0 +1,5 @@
+from .hole import HolE
+from .rescal import RESCAL
+from .transe import TransE
+from .base import StochasticTrainer, PairwiseStochasticTrainer
+from .actfun import afuns as activation_functions
